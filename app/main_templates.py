62b724657b7a50f952dc675ae70e4dcca315ee40
gautam-813 <dragufire@gmail.com>
2026-05-30 18:39:56 +0530

    Force-override Jinja2 cache via object.__setattr__

--- app/main_templates.py
+++ app/main_templates.py
@@ -7,24 +7,30 @@ class _SafeCache:
             hash(key)
         except TypeError:
             return default
-        return self._data.get(key, default) if hasattr(self, '_data') else default
+        return getattr(self, '_mapping', {}).get(key, default)
 
     def __setitem__(self, key, value):
         try:
             hash(key)
         except TypeError:
             return
-        if not hasattr(self, '_data'):
-            self._data = {}
-        self._data[key] = value
+        if not hasattr(self, '_mapping'):
+            self._mapping = {}
+        self._mapping[key] = value
 
     def __contains__(self, item):
         try:
             hash(item)
         except TypeError:
             return False
-        return item in getattr(self, '_data', {})
+        return item in getattr(self, '_mapping', {})
 
 
 templates = Jinja2Templates(directory="app/templates")
-templates.env._cache = _SafeCache()
+env = templates.env
+# Force-replace underlying cache objects to prevent unhashable key errors
+for attr in ('cache', '_cache'):
+    try:
+        object.__setattr__(env, attr, _SafeCache())
+    except (AttributeError, TypeError):
+        pass
