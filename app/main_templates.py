4d0e221e869d4ea2fa33bd06f9b3f8e4cefccf5a
gautam-813 <dragufire@gmail.com>
2026-05-30 18:36:26 +0530

    Replace Jinja2 cache with safe no-op cache to fix Python 3.14 bug

--- app/main_templates.py
+++ app/main_templates.py
@@ -1,5 +1,30 @@
 from fastapi.templating import Jinja2Templates
 
 
+class _SafeCache:
+    def get(self, key, default=None):
+        try:
+            hash(key)
+        except TypeError:
+            return default
+        return self._data.get(key, default) if hasattr(self, '_data') else default
+
+    def __setitem__(self, key, value):
+        try:
+            hash(key)
+        except TypeError:
+            return
+        if not hasattr(self, '_data'):
+            self._data = {}
+        self._data[key] = value
+
+    def __contains__(self, item):
+        try:
+            hash(item)
+        except TypeError:
+            return False
+        return item in getattr(self, '_data', {})
+
+
 templates = Jinja2Templates(directory="app/templates")
-templates.env.cache_size = 0
+templates.env._cache = _SafeCache()
