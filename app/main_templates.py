ea91b580f1e63542ba341d321bdf1a1655db10b3
gautam-813 <dragufire@gmail.com>
2026-05-30 18:30:20 +0530

    Fix templates: use simple Jinja2Templates with cache_size=0

--- app/main_templates.py
+++ app/main_templates.py
@@ -1,12 +1,4 @@
-from jinja2 import Environment, FileSystemLoader
-from starlette.templating import Jinja2Templates
-
-from app.core.config import settings
+from fastapi.templating import Jinja2Templates
 
 
-_env = Environment(
-    loader=FileSystemLoader("app/templates"),
-    cache_size=0 if settings.environment == "production" else 50,
-)
-templates = Jinja2Templates(env=_env)
-
+templates = Jinja2Templates(directory="app/templates", cache_size=0)
